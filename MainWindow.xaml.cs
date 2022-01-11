f4b86652d928e02f991de7d83ac1fb2ebc23aa64
Ebayparts <sanyav89@ukr.net>
2022-01-11 19:00:17 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pasteFromClipboardLearning1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<SpecificCar> carsList = new List<SpecificCar>
+            {
+                new SpecificCar {Year="1989", Make="Volkswagen", Model="Passat", BodyAndTrim="Base", EngAndTranny="1.8l L4 - Gas" },
+                new SpecificCar {Year="1990", Make="Mercedes", Model="C200" , BodyAndTrim="Base", EngAndTranny="1.8l L4 - Gas" },
+                new SpecificCar {Year="1991", Make="Audi", Model="100" , BodyAndTrim="Base", EngAndTranny="1.8l L4 - Gas" }
+            };
+            carsTable.ItemsSource = carsList;
+        }
+
+        private void btnInsertFromClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            this.txtGetData.Text = Clipboard.GetText();
+            //carsTable.DataContext = Clipboard.GetText();
+            carsTable.ItemsSource = Clipboard.GetText();
+        }
+    }
+}
---
+++ SpecificCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pasteFromClipboardLearning1
+{
+    public class SpecificCar
+    {
+        public string Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string BodyAndTrim { get; set; }
+        public string EngAndTranny { get; set; }
+
+    }
+}
